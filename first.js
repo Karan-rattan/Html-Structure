7b22b2cf07f27c82de7a228bb4f9bfa18e511916
Karan Rattan <karanrattan0420@gmail.com>
2024-11-19 16:05:45 -0700

    Added a button in the footer using javascript to view current date and time.

--- first.js
+++ first.js
@@ -1,3 +1,9 @@
+
+//to get alert message
 function showAlert(){
     alert("Welcome to my Website click Close to proceed");
+}
+//to show current date and time 
+function displayDate(){
+    document.getElementById('cdate').innerHTML = Date();
 }
