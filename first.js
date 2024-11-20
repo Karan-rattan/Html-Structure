f3b3c2a74864a1e4d95c9f0b5201142cba661f46
Karan Rattan <karanrattan0420@gmail.com>
2024-11-19 23:53:13 -0700

    deleted css that was extra and overwriten , created a greeting message with javascript and css that will wish my viewers

--- first.js
+++ first.js
@@ -7,3 +7,18 @@ function showAlert(){
 function displayDate(){
     document.getElementById('cdate').innerHTML = Date();
 }
+//to get greeting message according to time 
+function getGreeting() {
+    const currentHour = new Date().getHours();
+    let greetingMessage;
+    if (currentHour >= 5 && currentHour < 12) {
+        greetingMessage = "Good Morning viewers";
+    } else if (currentHour >= 12 && currentHour < 18) {
+        greetingMessage = "Good Afternoon viewers";
+    } else {
+        greetingMessage = "Good Evening viewers";
+    }
+    document.getElementById("greeting").innerHTML = greetingMessage;
+}
+window.onload = getGreeting;
+
